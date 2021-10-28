b865e0a515826734be4f0c78c8a1c1b28a3a5846
Johannes Loher <johannes.loher@fg4f.de>
2021-10-28 21:45:10 +0200

    ci: add minification for production builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,25 @@
 //
 // SPDX-License-Identifier: MIT
 
+const sourcemaps = require('rollup-plugin-sourcemaps');
 const typescript = require('rollup-plugin-typescript2');
+const { terser } = require('rollup-plugin-terser');
 
-module.exports = {
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
   input: 'src/module/foreground-drawings.ts',
   output: {
     dir: 'dist/module',
     format: 'es',
     sourcemap: true,
   },
-  plugins: [typescript({})],
+  plugins: [
+    sourcemaps(),
+    typescript({}),
+    process.env.NODE_ENV === 'production' && terser({ ecma: 2020, keep_fnames: true }),
+  ],
 };
+
+module.exports = config;
