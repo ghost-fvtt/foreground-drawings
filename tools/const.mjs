837716cc0ba0a27c2b0c1efa0bcd21ca41e84c20
Johannes Loher <johannes.loher@fg4f.de>
2021-12-07 01:23:52 +0100

    build: switch to pure rollup based build

---
+++ tools/const.mjs
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2021 Johannes Loher
+//
+// SPDX-License-Identifier: MIT
+
+export const name = 'foreground-drawings';
+export const sourceDirectory = './src';
+export const distDirectory = './dist';
+export const destinationDirectory = 'modules';
+export const foundryconfigFile = './foundryconfig.json';
